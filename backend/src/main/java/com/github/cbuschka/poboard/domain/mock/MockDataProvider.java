7f298b6c9c58b8b51bf6690e07e420855727550f
conni <cbuschka@gmail.com>
2021-04-12 17:01:49 +0200

    Switched from json to yaml.

--- backend/src/main/java/com/github/cbuschka/poboard/domain/mock/MockDataProvider.java
+++ backend/src/main/java/com/github/cbuschka/poboard/domain/mock/MockDataProvider.java
@@ -1,6 +1,7 @@
 package com.github.cbuschka.poboard.domain.mock;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -10,14 +11,16 @@ import java.io.IOException;
 @Component
 public class MockDataProvider
 {
-	@Value("classpath:mock-data.json")
+	@Value("classpath:mock-data.yaml")
 	private Resource mockDataResource;
 
 	public MockData getMockData()
 	{
 		try
 		{
-			return new ObjectMapper().readerFor(MockData.class).readValue(this.mockDataResource.getInputStream());
+			MockData mockData = new ObjectMapper(new YAMLFactory()).readerFor(MockData.class).readValue(this.mockDataResource.getInputStream());
+
+			return mockData;
 		}
 		catch (IOException ex)
 		{
