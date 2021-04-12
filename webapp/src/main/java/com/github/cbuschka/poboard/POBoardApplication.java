5c45e2062ade87f5efb67db43bd7e26e46b3aea9
conni <cbuschka@gmail.com>
2021-04-12 21:39:42 +0200

    Removed jpa/jdbc/flyway/postgres.

--- webapp/src/main/java/com/github/cbuschka/poboard/POBoardApplication.java
+++ webapp/src/main/java/com/github/cbuschka/poboard/POBoardApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value="classpath:backend-application.properties", ignoreResourceNotFound = false)
-@PropertySource(value="classpath:webapp-application.properties", ignoreResourceNotFound = false)
 public class POBoardApplication
 {
 	public static void main(String[] args)
