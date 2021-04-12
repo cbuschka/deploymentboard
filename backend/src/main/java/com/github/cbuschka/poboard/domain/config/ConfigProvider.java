46b30ce679536d64c6a3a015b77b72ff523a09c5
conni <cbuschka@gmail.com>
2021-04-12 17:17:07 +0200

    Config now loaded on startup.

--- backend/src/main/java/com/github/cbuschka/poboard/domain/config/ConfigProvider.java
+++ backend/src/main/java/com/github/cbuschka/poboard/domain/config/ConfigProvider.java
@@ -2,23 +2,43 @@ package com.github.cbuschka.poboard.domain.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class ConfigProvider
 {
-	@Value("classpath:config.yaml")
+	@Value("${poboard.config:classpath:config.yaml}")
 	private Resource configResource;
 
-	public Config getConfig()
+	private Config config;
+
+	@PostConstruct
+	public synchronized Config getConfig()
+	{
+		if (this.config == null)
+		{
+			this.config = loadConfig();
+		}
+
+		return this.config;
+	}
+
+	private Config loadConfig()
 	{
 		try
 		{
-			return new ObjectMapper(new YAMLFactory()).readerFor(Config.class).readValue(this.configResource.getInputStream());
+			Config config = new ObjectMapper(new YAMLFactory()).readerFor(Config.class).readValue(this.configResource.getInputStream());
+
+			log.info("Config loaded from {}.", this.configResource.getURI());
+
+			return config;
 		}
 		catch (IOException ex)
 		{
