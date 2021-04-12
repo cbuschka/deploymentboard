596556ac73e0763435532abe3082af14a2faa98d
conni <cbuschka@gmail.com>
2021-04-12 19:27:33 +0200

    Added basic auth support for http endpoints.

--- backend/src/main/java/com/github/cbuschka/poboard/domain/deployment/HttpEndpointHandler.java
+++ backend/src/main/java/com/github/cbuschka/poboard/domain/deployment/HttpEndpointHandler.java
@@ -1,5 +1,7 @@
 package com.github.cbuschka.poboard.domain.deployment;
 
+import com.github.cbuschka.poboard.domain.auth.AuthDomainService;
+import com.github.cbuschka.poboard.domain.auth.PasswordCredentials;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -7,11 +9,16 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
 
 @Component
 @Slf4j
 public class HttpEndpointHandler implements EndpointHandler
 {
+	@Autowired
+	private AuthDomainService authDomainService;
 	@Autowired
 	private DeploymentInfoExtractor deploymentInfoExtractor;
 
@@ -28,6 +35,7 @@ public class HttpEndpointHandler implements EndpointHandler
 		{
 			URL url = new URL(endpoint.getUrl());
 			HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+			addBasicAuthHeaderIfAvailable(url, httpConn);
 			httpConn.setDoInput(true);
 			int responseCode = httpConn.getResponseCode();
 			if (responseCode != 200)
@@ -44,5 +52,28 @@ public class HttpEndpointHandler implements EndpointHandler
 			return DeploymentInfo.unvailable(system, env);
 		}
 
+	}
+
+	private void addBasicAuthHeaderIfAvailable(URL url, HttpURLConnection httpConn)
+	{
+		if (url.getUserInfo() == null)
+		{
+			return;
+		}
+
+		List<PasswordCredentials> usernamePasswordCredentials = this.authDomainService.getUsernamePasswordCredentials(url.getUserInfo(), url.getHost());
+		if (usernamePasswordCredentials.isEmpty())
+		{
+			return;
+		}
+
+		if (usernamePasswordCredentials.size() > 1)
+		{
+			log.warn("Multiple password credentials avaialble for {}. Please fix.", url);
+		}
+
+		String usernameColonPassword = String.format("%s:%s", url.getUserInfo(), usernamePasswordCredentials.get(0).getPassword());
+		String authHeader = "Basic " + Base64.getEncoder().encodeToString(usernameColonPassword.getBytes(StandardCharsets.UTF_8));
+		httpConn.addRequestProperty("Authentication", authHeader);
 	}
 }
