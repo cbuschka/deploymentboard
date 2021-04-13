e04b14b2270d83d813c22bf1cd9299771a5ce073
conni <cbuschka@gmail.com>
2021-04-13 13:05:34 +0200

    Added test for GitChangeCollector.

--- backend/src/main/java/com/github/cbuschka/poboard/domain/scm/GitChangeCollector.java
+++ backend/src/main/java/com/github/cbuschka/poboard/domain/scm/GitChangeCollector.java
@@ -6,6 +6,7 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -17,18 +18,25 @@ public class GitChangeCollector
 {
 	public List<Change> collectChanges(Git git, String commitish, String optionalEndCommitish) throws Exception
 	{
+		ObjectId startObjectId = ObjectId.fromString(commitish);
 		ObjectId optionalEndObjectId = optionalEndCommitish != null ? ObjectId.fromString(optionalEndCommitish) : null;
+
+		return collectChangesByCommitWalk(git, startObjectId, optionalEndObjectId);
+	}
+
+	private List<Change> collectChangesByCommitWalk(Git git, ObjectId startObjectId, ObjectId optionalEndObjectId) throws IOException
+	{
 		List<Change> changes = new ArrayList<>();
 		try (RevWalk walk = new RevWalk(git.getRepository()))
 		{
-			RevCommit startCommit = walk.parseCommit(ObjectId.fromString(commitish));
-			List<RevCommit> traversalQueue = new LinkedList<>();
+			RevCommit startCommit = walk.parseCommit(startObjectId);
+			List<ObjectId> traversalQueue = new LinkedList<>();
 			traversalQueue.add(0, startCommit);
 
 			Set<ObjectId> seenSet = new HashSet<>();
 			while (!traversalQueue.isEmpty())
 			{
-				RevCommit commit = traversalQueue.remove(0);
+				RevCommit commit = walk.parseCommit(traversalQueue.remove(0));
 				if (optionalEndObjectId != null && optionalEndObjectId.equals(commit.getId()))
 				{
 					continue;
@@ -39,11 +47,13 @@ public class GitChangeCollector
 					continue;
 				}
 
+				commit = walk.parseCommit(commit.getId());
+
 				String message = commit.getRawBuffer() != null ? commit.getFullMessage() : "";
 				for (int i = 0; commit.getParents() != null && i < commit.getParentCount(); ++i)
 				{
 					RevCommit parent = commit.getParent(commit.getParentCount() - i - 1);
-					traversalQueue.add(parent);
+					traversalQueue.add(parent.getId());
 				}
 				changes.add(new Change(commit.getId().toString(), message));
 				seenSet.add(commit.getId());
