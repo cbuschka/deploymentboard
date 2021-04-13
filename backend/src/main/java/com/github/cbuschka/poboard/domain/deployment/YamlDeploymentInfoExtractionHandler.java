3e8f3801655f5ce005ff27321abae6e71223e2cf
conni <cbuschka@gmail.com>
2021-04-13 13:47:38 +0200

    #4 Added support for deployment info format yaml.

---
+++ backend/src/main/java/com/github/cbuschka/poboard/domain/deployment/YamlDeploymentInfoExtractionHandler.java
@@ -0,0 +1,51 @@
+package com.github.cbuschka.poboard.domain.deployment;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Component
+public class YamlDeploymentInfoExtractionHandler implements DeploymentInfoExtractionHandler
+{
+	private final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+	@Override
+	public boolean handles(Endpoint endpoint)
+	{
+		return "yaml".equals(endpoint.getFormat()) || (endpoint.getUrl() != null && (endpoint.getUrl().endsWith(".yaml") || endpoint.getUrl().endsWith(".yml")));
+	}
+
+	@Override
+	public DeploymentInfo extractDeploymentInfoFrom(Endpoint endpoint, InputStream in, String system, String env) throws IOException
+	{
+		JsonNode jsonNode = this.objectMapper.reader().readTree(in);
+		if (jsonNode == null)
+		{
+			return DeploymentInfo.unvailable(system, env);
+		}
+
+		String version = getStringFrom(jsonNode, DeploymentInfoExtractor.VERSION_ALIASES);
+		String commitish = getStringFrom(jsonNode, DeploymentInfoExtractor.COMMITISH_ALIASES);
+		String branch = getStringFrom(jsonNode, DeploymentInfoExtractor.BRANCH_ALIASES);
+		return new DeploymentInfo(DeploymentStatus.AVAILABLE, system, env, commitish, version, branch);
+	}
+
+	private String getStringFrom(JsonNode jsonNode, List<String> keys)
+	{
+		for (String key : keys)
+		{
+			JsonNode stringNode = jsonNode.at("/" + key);
+			if (stringNode != null && stringNode.isTextual())
+			{
+				return stringNode.asText();
+			}
+		}
+
+		return null;
+	}
+}
