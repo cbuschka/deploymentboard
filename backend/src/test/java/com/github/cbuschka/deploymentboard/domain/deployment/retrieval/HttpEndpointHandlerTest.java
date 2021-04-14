862e616d73bfca2e9fc693784f88ac9f829c4b13
conni <cbuschka@gmail.com>
2021-04-14 17:20:52 +0200

    Added some endpoint handler tests.

---
+++ backend/src/test/java/com/github/cbuschka/deploymentboard/domain/deployment/retrieval/HttpEndpointHandlerTest.java
@@ -0,0 +1,49 @@
+package com.github.cbuschka.deploymentboard.domain.deployment.retrieval;
+
+import com.github.cbuschka.deploymentboard.domain.deployment.Endpoint;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class HttpEndpointHandlerTest
+{
+	@InjectMocks
+	private HttpEndpointHandler handler;
+
+	@Mock
+	private Endpoint endpoint;
+
+	private boolean handles;
+
+	@Test
+	void deniesHandlingNullUrl()
+	{
+		givenEndpointWithoutUrl();
+
+		whenAskedIfEndpointHandled();
+
+		thenHandlingIsDenied();
+	}
+
+	private void thenHandlingIsDenied()
+	{
+		assertThat(this.handles).isFalse();
+	}
+
+	private void whenAskedIfEndpointHandled()
+	{
+		this.handles = handler.handles(endpoint);
+	}
+
+	private void givenEndpointWithoutUrl()
+	{
+		when(this.endpoint.getUrl()).thenReturn(null);
+	}
+
+}
