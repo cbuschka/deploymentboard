d7dbd4c80f705f74fa148e4870f91d0c801f1ec8
conni <cbuschka@gmail.com>
2021-04-29 21:38:21 +0200

    Added link support to issue.

--- frontend/src/main/frontend/src/ui/dashboard/Issue.jsx
+++ frontend/src/main/frontend/src/ui/dashboard/Issue.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import './Issue.css';
 import classnames from 'classnames';
 
-export const Issue = ({issue, className}) => {
-    const {issueNo, status, title} = issue;
-    return <span className={classnames("Issue", className, status)}>
+export class Issue extends React.Component {
+
+    render() {
+        const {issue: {issueNo, status, title, link}, className} = this.props;
+        return <span className={classnames("Issue", className, status, !!link ? "Issue__linked" : null)}
+                     onClick={this._onClick}>
         <span className="Issue__title_issueNo">{issueNo}</span>
         <span className="Issue__title_issueStatus">&nbsp;({status})</span>
-        {!!title ? <span className="Issue__title_issueSummary">{title}</span> : null}</span>;
-};
+            {!!title ? <span className="Issue__title_issueSummary">{title}</span> : null}</span>;
+    }
+
+    _onClick = () => {
+        const {issue: {link}} = this.props;
+        window.open(link, '_blank').focus();
+    }
+}
