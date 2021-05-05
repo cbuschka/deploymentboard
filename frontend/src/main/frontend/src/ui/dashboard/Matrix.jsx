786f81753bdaa9e4e62526db9573daaa9a9a5894
conni <cbuschka@gmail.com>
2021-05-05 06:19:17 +0200

    Removed unused lock status.

--- frontend/src/main/frontend/src/ui/dashboard/Matrix.jsx
+++ frontend/src/main/frontend/src/ui/dashboard/Matrix.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './Matrix.css';
 import classnames from 'classnames';
 import {Table} from "reactstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faLock, faLockOpen} from '@fortawesome/free-solid-svg-icons'
 import {Issue} from "./Issue";
 
 const InfoBlock = ({text}) => {
@@ -13,27 +11,6 @@ const InfoBlock = ({text}) => {
 
     return <span className="Matrix_InfoBlock">{text}</span>;
 };
-
-const LockStatus = ({value, disabled = true}) => {
-    if (!value) {
-        return null;
-    }
-
-    switch (value) {
-        case "UNLOCKED":
-            return <div className={classnames("LockStatus", value, disabled ? "disabled" : "")}><FontAwesomeIcon
-                icon={faLockOpen}/></div>;
-        case "LOCKED":
-            return <div className={classnames("LockStatus", value, disabled ? "disabled" : "")}><FontAwesomeIcon
-                icon={faLock}/></div>;
-        case "NOT_LOCKABLE":
-            return <div className={classnames("LockStatus", value, "disabled")}><FontAwesomeIcon
-                icon={faLock}/></div>;
-        default:
-            return "";
-    }
-};
-
 
 const StatusBadge = ({status}) => {
     return <div className={classnames("Matrix_StatusBadge", status)}>{status}</div>;
@@ -60,13 +37,12 @@ class DeploymentInfo extends React.Component {
             version = "n/a",
             branch = "n/a",
             commitish = "n/a",
-            buildTimestamp,
-            lockStatus
+            buildTimestamp
         } = systemEnv;
 
         if (!ok) {
             return (<>
-                <div className="Matrix_system_environment_toolLine"><LockStatus value={lockStatus}/></div>
+                <div className="Matrix_system_environment_toolLine">&nbsp;</div>
                 <div className="Matrix_system_environment_content">
                     <span className="Matrix_system_environment_headline"><StatusBadge status={status}/></span>
                     <div className="Matrix_system_environment_warning">{message || "n/a"}</div>
@@ -75,7 +51,7 @@ class DeploymentInfo extends React.Component {
         }
 
         return (<>
-            <div className="Matrix_system_environment_toolLine"><LockStatus value={lockStatus}/></div>
+            <div className="Matrix_system_environment_toolLine">&nbsp;</div>
             <div className="Matrix_system_environment_content">
                 <span className="Matrix_system_environment_headline"><StatusBadge
                     status={status}/>&nbsp;{version}</span>
